c4b26632a39997115b61dfe52dd7285a8b7ac804
Yuval Garti <yuvalgarti@gmail.com>
2021-08-01 03:29:58 +0300

    Changed implementation & added firebase

--- tweet_screenshot.py
+++ tweet_screenshot.py
@@ -3,9 +3,12 @@ import pyppeteer
 import tweepy
 import os
 import time
+import pyrebase
+
 
 class NoRetweetException(Exception):
     pass
+
 
 async def screenshot_tweet(api, tweet_id, path_to_image):
     tweet = api.get_status(tweet_id)
@@ -20,53 +23,44 @@ async def screenshot_tweet(api, tweet_id, path_to_image):
     tweet_frame = await page.querySelector('iframe')
     await tweet_frame.screenshot({'path': path_to_image})
     await browser.close()
-    
 
-async def screenshot_quoted_tweet(api, tweet_id, path_to_image):
-    tweet = api.get_status(tweet_id)
-    if hasattr(tweet, 'quoted_status'):
-        await screenshot_tweet(api, tweet.quoted_status.id_str, path_to_image)
-    else:
-        raise NoRetweetException('No quoted tweet')
-        
-async def reply_to_tweet_with_screenshot(api, tweet_id, path_to_image):
-    tweet = api.get_status(tweet_id)
-    user = tweet.user
-    print('screenshotting tweet')
-    try:
-        await screenshot_quoted_tweet(api, tweet_id, path_to_image)
-        print('uploading tweet')
-        api.update_with_media(path_to_image, status='@'+user.screen_name, in_reply_to_status_id=tweet_id)
-        if os.path.exists(path_to_image):
-            os.remove(path_to_image)
+
+async def reply_to_mention_with_screenshot(api, mention, tweet_to_screenshot):
+    path_to_file = tweet_to_screenshot.id_str + '.png'
+    await screenshot_tweet(api, tweet_to_screenshot.id, path_to_file)
+    api.update_with_media(path_to_file, status='@' + mention.user.screen_name, in_reply_to_status_id=mention.id)
+    print('path_to_file: {}, status: @{}, in_reply_to_status_id: {}'.format(path_to_file, mention.user.screen_name,
+                                                                            mention.id))
+    if os.path.exists(path_to_file):
+        os.remove(path_to_file)
+
+
+async def blocked_retweet(api, mention):
+    if mention.in_reply_to_status_id:
+        viewed_tweet = api.get_status(mention.in_reply_to_status_id)
+        if hasattr(viewed_tweet, 'quoted_status'):
+            print('This is a retweet')
+            blocked_tweet = api.get_status(viewed_tweet.quoted_status.id)
+            await reply_to_mention_with_screenshot(api, mention, blocked_tweet)
         else:
-            print(path_to_image + ' does not exist')
-    except NoRetweetException as exp:
-        msg = 'אין מה לצלם פה...'
-        api.update_status(status='@'+user.screen_name + ' ' + msg, in_reply_to_status_id=tweet_id)
+            print('This is a comment')
+            await blocked_comment(api, mention)
+            api.update_status(status='@' + mention.user.screen_name + ' ' + msg, in_reply_to_status_id=mention.id)
 
 
-async def reply_to_mention_with_screenshot(api, mention, path_to_image):
-    tweet = api.get_status(mention.id)
-    user = tweet.user
-    try:
-        print('screenshotting quoted tweet of tweet id: ' + str(mention.in_reply_to_status_id))
-        await screenshot_quoted_tweet(api, mention.in_reply_to_status_id, path_to_image)
-        print('uploading screenshot in reply to mention id: ' + mention.id_str)
-        api.update_with_media(path_to_image, status='@'+user.screen_name, in_reply_to_status_id=mention.id)
-        if os.path.exists(path_to_image):
-            os.remove(path_to_image)
+async def blocked_comment(api, mention):
+    if mention.in_reply_to_status_id:
+        viewed_tweet = api.get_status(mention.in_reply_to_status_id)
+        if viewed_tweet.in_reply_to_status_id:
+            blocked_tweet = api.get_status(viewed_tweet.in_reply_to_status_id)
+            await reply_to_mention_with_screenshot(api, mention, blocked_tweet)
         else:
-            print(path_to_image + ' does not exist')
-    except NoRetweetException as exp:
-        msg = 'אין מה לצלם פה...'
-        api.update_status(status='@'+user.screen_name + ' ' + msg, in_reply_to_status_id=mention.id)
+            msg = 'לצערי אין תגובה ואין ריטוויט  (או שהמשתמש נעול)'
+            print(msg)
 
 
-def run(api):
-    last_mention = 1
-    with open('last_mention.txt', 'r') as last_mention_file:
-        last_mention = int(last_mention_file.read())
+def run(api, db):
+    last_mention = int(db.child("last_mention_id").get().val())
     max_mention_id = last_mention
     while True:
         print('getting mentions since ' + str(max_mention_id))
@@ -76,13 +70,10 @@ def run(api):
             if last_mention > max_mention_id:
                 max_mention_id = last_mention
             print('Mention by: @' + mention.user.screen_name)
-            asyncio.get_event_loop().run_until_complete(reply_to_mention_with_screenshot(api, mention, mention.id_str + '.png'))
-        print('writing ' + str(max_mention_id) + ' to last_mention_file.txt')
-        with open('last_mention.txt', 'w') as last_mention_file:
-            last_mention_file.write(str(max_mention_id))
+            asyncio.get_event_loop().run_until_complete(blocked_retweet(api, mention))
+        print('writing ' + str(max_mention_id) + ' to DB')
+        db.child("last_mention_id").set(str(max_mention_id))
         time.sleep(15)
-
-
 
 
 pyppeteer.chromium_downloader.download_chromium()
@@ -90,6 +81,15 @@ pyppeteer.chromium_downloader.download_chromium()
 auth = tweepy.OAuthHandler(os.environ['SCREENSHOT_CONSUMER_KEY'], os.environ['SCREENSHOT_CONSUMER_VALUE'])
 auth.set_access_token(os.environ['SCREENSHOT_ACCESS_TOKEN_KEY'], os.environ['SCREENSHOT_ACCESS_TOKEN_VALUE'])
 
-api = tweepy.API(auth, wait_on_rate_limit=True)
+firebase_config = {
+    "apiKey": os.environ['FIREBASE_API_KEY'],
+    "authDomain": os.environ['FIREBASE_AUTH_DOMAIN'],
+    "databaseURL": os.environ['FIREBASE_DB_URL'],
+    "storageBucket": os.environ['FIREBASE_STORAGE_BUCKET']
+}
 
-run(api)
+
+tweepy_api = tweepy.API(auth, wait_on_rate_limit=True)
+firebase = pyrebase.initialize_app(firebase_config)
+
+run(tweepy_api, firebase.database())
