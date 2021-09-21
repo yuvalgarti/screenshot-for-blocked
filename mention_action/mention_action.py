cea38f24fea91dfae4b746842decdb850c4412a1
Yuval Garti <yuvalgarti@gmail.com>
2021-09-22 00:05:30 +0300

    Use mention_bot package

--- mention_action/mention_action.py
+++
@@ -1,6 +0,0 @@
-class MentionAction:
-    async def run(self, mention):
-        pass
-
-    def setup(self):
-        pass
--- mention_handler/services/last_mention_service.py
+++
@@ -1,6 +0,0 @@
-class LastMentionService:
-    def get_last_mention(self):
-        pass
-
-    def set_last_mention(self, last_mention):
-        pass
